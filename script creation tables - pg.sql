f4bb2fac8effa3d7c90a7e6259afab1f7a711f19
nabil aici <cybernaboo@yahoo.fr>
2022-03-21 23:47:21 +0100

    modif sql serial id

--- script creation tables - pg.sql
+++ script creation tables - pg.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS public.taches;
 
 CREATE TABLE IF NOT EXISTS public.taches
 (
-    id integer NOT NULL DEFAULT nextval('taches_id_seq'::regclass),
+    id serial,
     tache character varying(30) COLLATE pg_catalog."default" NOT NULL,
     echeance date,
     cloture boolean NOT NULL DEFAULT false,
